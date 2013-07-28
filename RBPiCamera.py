b0035da53382bab60927f2086c7cf572c39bb4ae
Roberto Calvo <rocapal@gmail.com>
2013-07-28 23:13:21 +0000

    first version of WTL to RaspBerryPi

---
+++ RBPiCamera.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2013
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+#  Authors : Roberto Calvo <rocapal at gmail dot com>
+
+
+from subprocess import call
+import StringIO
+from json import JSONEncoder
+from django.utils import simplejson
+import socket
+import threading
+
+IMAGE_FILE_PATH = "/tmp/image.jpg"
+
+RBPI_PHOTO_COMMAND = "raspistill"
+RBPI_VIDEO_COMMAND = "raspivid"
+
+VLC_STREAMING_COMMAND = " | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264"
+VLC_STREAMING_URL = "http://%s:8090/"
+
+control_streaming_th = None 
+
+disable_args = ['none','false', 'off']
+
+
+
+def parse_args (args_list, timeout = 0):
+
+	args = ""
+
+	if "timeout" is not args_list:
+		args = args + "--timeout " + str(timeout) + " "
+
+
+	for arg in args_list:
+		if (arg["argument"] == "true"):
+			args = args + "--" + arg["name"] + " "
+			continue
+		if (arg["argument"] in disable_args):
+			continue
+                        
+		args = args + "--" + arg["name"] + " " + arg["argument"] + " "
+
+	return args
+
+def snap_photo (args_list, image_path = None):
+
+	res_image = ""
+	if (image_path != None):
+		res_image = image_path
+	else:
+		res_image = IMAGE_FILE_PATH
+	
+	args = "--output " + res_image + " "  + parse_args(args_list, 2000)
+
+	command = RBPI_PHOTO_COMMAND + " " + args
+	print command
+	return_code = call(command, shell=True);
+	if (return_code == 0):
+		return res_image 
+	else:
+		return None
+
+
+def launch_cmd (command):
+	code = call (command, shell=True)
+
+def start_streaming (args_list):
+
+	global control_streaming_th 
+	res = {}
+	args = " -o - " + parse_args(args_list, 9999999)
+	command = RBPI_VIDEO_COMMAND + " " + args + " " + VLC_STREAMING_COMMAND
+
+	print command 
+
+	try:
+		if (control_streaming_th == None):
+			control_streaming_th = 1
+			streaming_th  = threading.Thread(target = launch_cmd, args=[command])
+			streaming_th.setDaemon(True)
+			streaming_th.start()
+
+		res["code"] = 200
+		res["streaming_url"] = VLC_STREAMING_URL % (get_ip())
+		
+	except:
+		res["code"] = 500
+		res["msg"] = "Error while streaming was initialized!"
+		res["streaming_url"] = ""
+		
+
+	return res
+
+def stop_streaming():
+
+	global control_streaming_th 
+
+	cmds = {'vlc',RBPI_VIDEO_COMMAND}
+
+	for cmd in cmds:
+		print "killing " + cmd
+		kill_command = "ps aux | grep " + cmd + " | grep -v grep | tr -s ' ' | cut -d' ' -f2 | tr -s '\n' ' ' | sed -e s/^/kill\ -9\ /g | bash"
+		call (kill_command, shell=True)
+
+	control_streaming_th = None
+
+
+
+def get_ip ():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("gmail.com",80))
+	ip = (s.getsockname()[0])
+	s.close()
+	return ip
+
---
+++ WTLv2.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2013
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+#  Authors : Roberto Calvo <rocapal at gmail dot com>
+#            Dan Mandle
+
+import os
+from gps import *
+from time import *
+import time
+import threading
+from sys import stdout 
+from temp import read_temp
+from RBPiCamera import snap_photo
+import RPi.GPIO as GPIO
+
+gpsd = None 
+
+RED = 23
+GREEN = 24
+BLUE = 25
+ 
+os.system('clear') 
+ 
+class GpsPoller(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    global gpsd 
+    gpsd = gps(mode=WATCH_ENABLE) 
+    self.current_value = None
+    self.running = True 
+ 
+  def run(self):
+    global gpsd
+    while gpsp.running:
+      gpsd.next()
+
+
+def init ():
+  
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(RED, GPIO.OUT)
+  GPIO.setup(GREEN, GPIO.OUT)
+  GPIO.setup(BLUE, GPIO.OUT)
+
+  for i in range(0,2):
+    GPIO.output(RED,False)
+    GPIO.output(GREEN,True)
+    GPIO.output(BLUE,True)
+    time.sleep(1)
+    GPIO.output(RED,True)
+    GPIO.output(GREEN,False)
+    GPIO.output(BLUE,True)
+    time.sleep(1)
+
+  GPIO.output(RED,True)
+  GPIO.output(GREEN,True)
+  GPIO.output(BLUE,True)
+
+def led_gps_search (status):
+  GPIO.output(RED,True)
+  GPIO.output(GREEN,True)
+  GPIO.output(BLUE,status)
+
+def led_data (status):
+  GPIO.output(RED,True)
+  GPIO.output(GREEN,status)
+  GPIO.output(BLUE,True)
+
+def save_data (file_name, temp):
+
+  data_file = open(file_name, 'w')  
+
+  data_file.write(gpsd.utc + "\n")
+  data_file.write(str(gpsd.fix.latitude) + "\n")
+  data_file.write(str(gpsd.fix.longitude) + "\n")
+  data_file.write(str(gpsd.fix.altitude) + "\n")
+  data_file.write(str(gpsd.fix.speed) + "\n")
+  data_file.write(str(temp[0]) + "\n")
+
+  data_file.close()
+
+def get_file_name (datetime):
+
+  try:
+    date = datetime.split("T")[0].replace("-",".")
+    time = datetime.split("T")[1].split(".")[0].replace(":",".")
+    return date + "-" + time
+  except:
+    print "Not well formed the filename with: " + datetime
+    return None
+ 
+if __name__ == '__main__':
+
+  init()
+
+  gpsp = GpsPoller()
+  try:
+    gpsp.start() 
+    
+
+    list = ['|','/','-','\\']
+    c = 0
+
+    SLEEP_DATA = 15
+    DIR = "/home/pi/WTLv2-data/"
+
+    while True:
+
+      if (gpsd.fix.mode	== MODE_NO_FIX):
+        stdout.write("\rWaiting for FIX: %s" % list[c])
+        
+        c = c+1
+        if (c>2):
+          c=0
+
+        stdout.flush()
+        led_gps_search(False)
+        time.sleep(1)        
+        led_gps_search(True)
+        time.sleep(1)
+
+      else:
+
+        os.system('clear')
+        led_data(False);
+
+        print
+        print ' GPS reading'
+        print '----------------------------------------'
+        print 'latitude    ' , gpsd.fix.latitude
+        print 'longitude   ' , gpsd.fix.longitude
+        print 'time utc    ' , gpsd.utc
+        print 'altitude (m)' , gpsd.fix.altitude
+        print 'speed (m/s) ' , gpsd.fix.speed
+        print 'mode        ' , gpsd.fix.mode
+        print ' '
+
+        # Get Temperature
+        temp =  read_temp()
+        print ("Temperature: %.2f" % temp[0])
+
+        # Build filename to save data
+        file_name = get_file_name(gpsd.utc)
+
+        # Save the picture
+        args = []
+        arg1 = {"name": "width" , "argument": "640"}
+        arg2 = {"name": "height", "argument": "480"}
+	arg3 = {"name": "exposure", "argument": "auto"}
+        args.append(arg1);
+        args.append(arg2);
+	args.append(arg3);
+
+        snap_photo(args, DIR + file_name + "I")
+
+        # Save the data
+        save_data(DIR + file_name + "D", temp)
+
+        print "Save data and picture: " + DIR + file_name + "[I|D]"
+        
+        os.system("sync")
+        time.sleep(1)
+        led_data(True)
+        time.sleep(SLEEP_DATA) #set to whatever
+ 
+  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print "\nKilling Thread..."
+    
+    GPIO.output(RED,True)
+    GPIO.output(GREEN,True)
+    GPIO.output(BLUE,True)
+    gpsp.running = False
+    gpsp.join() # wait for the thread to finish what it's doing
+  print "Done.\nExiting."
